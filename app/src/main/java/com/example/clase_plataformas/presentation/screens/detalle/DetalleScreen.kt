2f301201f436438a71120092dbaeb7d94478954c
Alvaro Molina <amolinaeffio@gmail.com>
2026-09-08 21:01:18 -0500

    Se agrega diseño nuevo a los textos y al ícono

--- app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
+++ app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
@@ -2,8 +2,10 @@ package com.example.clase_plataformas.presentation.screens.detalle
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Icon
@@ -12,21 +14,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DetalleScreen(){
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFCACAEF))
+            .background(Color(0xFFD4D4EE))
             .padding(all = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Cien Años de Soledad")
-        Icon(
-            Icons.Outlined.Edit,
-            "Icono_nonmbres")
-        Text("Gabriel García Márquez")
+        Text(
+            text = "Cien Años de Soledad",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Serif,
+            color = Color(0xFF101140)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                Icons.Outlined.Edit,
+                "Icono_nonmbres",
+                tint = Color(0xFF3823A8),
+                modifier = Modifier.size(14.dp)
+            )
+            Text(
+                text = "Gabriel García Márquez",
+                fontSize = 14.sp,
+                color = Color(0xFF555B6E),
+                fontWeight = FontWeight.Medium
+            )
+        }
     }
 }
