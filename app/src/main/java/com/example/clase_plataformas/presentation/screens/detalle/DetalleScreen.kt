2e6332eed1b015f3eb6e213eda0b04e02622e66d
Alvaro Molina <amolinaeffio@gmail.com>
2026-09-08 22:00:25 -0500

    Se elimnan import innecesarios

--- app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
+++ app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
@@ -1,6 +1,5 @@
 package com.example.clase_plataformas.presentation.screens.detalle
 
-import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
