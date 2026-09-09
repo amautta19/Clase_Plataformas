ccbd8cd9dc4edd9e81c2334df5026384515fc431
Alvaro Molina <amolinaeffio@gmail.com>
2026-09-08 21:44:33 -0500

    Se agrega la imagen y diseño inicial

--- app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
+++ app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
@@ -1,25 +1,34 @@
 package com.example.clase_plataformas.presentation.screens.detalle
 
+import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.clase_plataformas.R
 
 @Composable
 fun DetalleScreen(){
@@ -30,6 +39,21 @@ fun DetalleScreen(){
             .padding(all = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Card(
+            modifier = Modifier.size(width = 180.dp, height = 240.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            shape = RoundedCornerShape(size = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.libro),
+                contentDescription = "Imagen de libro",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
         Text(
             text = "Cien Años de Soledad",
             fontSize = 22.sp,
