e19d56b5898f584a3d8041f469d6130bb79d5ead
Alvaro Molina <amolinaeffio@gmail.com>
2026-09-08 22:56:00 -0500

    Se agrega las botones al final y se activa el skroll

--- app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
+++ app/src/main/java/com/example/clase_plataformas/presentation/screens/detalle/DetalleScreen.kt
@@ -14,14 +14,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Label
 import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -49,7 +55,7 @@ fun DetalleScreen(){
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFD4D4EE))
-            .padding(all = 16.dp),
+            .padding(all = 16.dp).verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Card(
@@ -217,5 +223,36 @@ fun DetalleScreen(){
                 MetaBadge(text = "Idioma: Español")
             }
         }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Sección de botones
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1E1093)),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(44.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = null, modifier = Modifier.size(16.dp))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Editar", fontWeight = FontWeight.Bold)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFDCDC)),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(44.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = null, tint = Color(0xFFB3261E), modifier = Modifier.size(16.dp))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Eliminar", color = Color(0xFFB3261E), fontWeight = FontWeight.Bold)
+        }
+
     }
 }
